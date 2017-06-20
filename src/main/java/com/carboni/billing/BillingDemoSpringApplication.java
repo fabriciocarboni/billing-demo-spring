b8b7a4d38d12934a203975f7bb25c3c4ba2e9e83
Fabricio Carboni <carboni@gmail.com>
2017-06-20 16:44:18 -0300

    Initial Commit

---
+++ src/main/java/com/carboni/billing/BillingDemoSpringApplication.java
@@ -0,0 +1,12 @@
+package com.carboni.billing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingDemoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingDemoSpringApplication.class, args);
+	}
+}
