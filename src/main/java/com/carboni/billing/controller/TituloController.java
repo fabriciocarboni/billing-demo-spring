406d83c0b9dd9eea212fea746e9468d23691b408
Fabricio Carboni <carboni@gmail.com>
2017-06-22 00:41:42 -0300

    Update

--- src/main/java/com/carboni/billing/controller/TituloController.java
+++ src/main/java/com/carboni/billing/controller/TituloController.java
@@ -1,12 +1,17 @@
 package com.carboni.billing.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.carboni.billing.model.StatusTitulo;
 import com.carboni.billing.model.Titulo;
 import com.carboni.billing.repository.Titulos;
 
@@ -19,8 +24,12 @@ public class TituloController {
 	private Titulos titulos;
 	
 	@GetMapping("/novo")
-	public String novo() {
-		return "CadastroTitulo";
+	public ModelAndView novo() {
+		
+		//Ao acessar esse endpoint, é necessario deixar disponivel o combo do campo status com todos os Enums
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		return mv;
+		
 	}
 	
 
@@ -31,8 +40,16 @@ public class TituloController {
 		titulos.save(titulo); //salva no banco de dados
 		
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
-		mv.addObject("mensagem","Titulo salvo com sucesso!"); // Adciona msg no modelAndView para passar para o html CadastroTitulo.html
+		mv.addObject("mensagem","Titulo salvo com sucesso!"); // Adiciona msg no modelAndView para passar para o html CadastroTitulo.html
 		return mv;
+	}
+	
+	//Criado dentro de ModelAtribute para que o controller sempre tenha este objeto disponivel na view
+	//StatusTitulo é o enum. retorna o array com os valores todosStatusTitulo
+	// O parametro passado em @ModelAttribute é o nome que será iterado pelo thymeleaf na view
+	@ModelAttribute("todosStatusTitulo")
+	public List<StatusTitulo> todosStatusTitulo() {
+		return Arrays.asList(StatusTitulo.values());
 	}
 
 }
