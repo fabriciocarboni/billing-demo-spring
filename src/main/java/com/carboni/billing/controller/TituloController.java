bde0af92c71b3699d54323df478c61526fa6e75b
Fabricio Carboni <carboni@gmail.com>
2017-06-26 23:10:16 -0300

    Implementando a edição

--- src/main/java/com/carboni/billing/controller/TituloController.java
+++ src/main/java/com/carboni/billing/controller/TituloController.java
@@ -9,6 +9,7 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -22,6 +23,8 @@ import com.carboni.billing.repository.Titulos;
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
+	
+	private static final String CADASTRO_VIEW = "CadastroTitulo";
 
 	@Autowired // injetando a instancia do repositório Titulos
 	private Titulos titulos;
@@ -30,7 +33,7 @@ public class TituloController {
 	public ModelAndView novo() {
 		
 		//Ao acessar esse endpoint, é necessario deixar disponivel o combo do campo status com todos os Enums
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
 		
 		//Adiciona neste controller o objeto titulo para ser acessado na pagina CadastroTitulo.
 		//Esse objeto será acessado através desta tag html <form class="form-horizontal" method="POST" action="/titulos" th:object="${titulo}">
@@ -43,12 +46,12 @@ public class TituloController {
 	@PostMapping
 	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) { //Indica ao spring para validar o classe Titulo, de acordo com as regras onde tiver anotacoes de validation como @NotNull por exemplo
 		if (errors.hasErrors()) {
-			return "CadastroTitulo"; //Aqui retorna a pagina html CadastroTitulo.html
+			return CADASTRO_VIEW; //Aqui retorna a pagina html CadastroTitulo.html
 		}
 		
 		titulos.save(titulo); //salva no banco de dados
 		attributes.addFlashAttribute("mensagem","Titulo salvo com sucesso!"); // Adiciona a msg de cadastro com sucesso ao redirecionar para /titulos/novo após clicar no botao salvar
-		return "redirect:/titulos/novo"; //Aqui retorna uma url
+		return "redirect:/titulos/novo"; //Aqui retorna uma url redirecinando para titulos/novo
 	}
 	
 	@GetMapping //Nao é necessário colocar o endereço pois já esse controller deve retornar a pesquisa de titulos assim que o request chega em /titulos, que esta mapeado já na linha 20
@@ -57,7 +60,16 @@ public class TituloController {
 		ModelAndView mv = new ModelAndView("PesquisaTitulos");
 		mv.addObject("titulos",todosTitulos);
 		return mv;
+	}
+	
+	@GetMapping("{codigo}")
+	//public ModelAndView edicao(@PathVariable Long codigo) {
+	public ModelAndView edicao(@PathVariable("codigo") Titulo titulo) { //ao inves de usar a linha abaixo para procurar o codigo, o spring ja faz isso automaticamente se passarmos o codigo para o objeto Titulo
+		//Titulo titulo = titulos.findOne(codigo); //recupera do banco de dados o codigo informado no link
 		
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		mv.addObject(titulo); //passa para view CadastroTitulo.html o codigo para edicao.
+		return mv;
 	}
 	
 	
