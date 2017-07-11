ad80852ef54b4bfb163b5a521ba1bb4a031e2e40
Fabricio Carboni <fabricio.carboni@telefonica.com>
2017-07-11 18:07:14 -0300

    Adding responsive design

--- src/main/java/com/carboni/billing/BillingDemoSpringApplication.java
+++ src/main/java/com/carboni/billing/BillingDemoSpringApplication.java
@@ -6,7 +6,10 @@ import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 
@@ -20,5 +23,15 @@ public class BillingDemoSpringApplication {
 	@Bean
 	public LocaleResolver localeResolver() {
 		return new FixedLocaleResolver(new Locale("pt", "BR"));		
+	}
+	
+	@Configuration
+	public static class MvcConfig extends WebMvcConfigurerAdapter {
+	
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addRedirectViewController("/", "/titulos");
+		}
+		
 	}
 }
